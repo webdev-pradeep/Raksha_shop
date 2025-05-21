88a81244fcb9b89703dd870b70d66538e0b5616e
Pradeep <webdevpradeep0101@gmail.com>
2025-05-21 02:46:42 -1200

    loading in text add

--- src/app/(mainLayout)/page.jsx
+++ src/app/(mainLayout)/page.jsx
@@ -65,9 +65,11 @@ const HomePage = () => {
       </div>
 
       {loading && (
-        <div className="flex items-center justify-center bg-white fixed inset-0 z-10 h-screen w-full">
+        <div className="flex items-center justify-center bg-amber-300 fixed inset-0 z-10 h-screen w-full ">
           <div className="relative">
-            <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+            <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200 text-5xl text-center">
+              𝓡𝓟
+            </div>
             <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin"></div>
           </div>
         </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,48 +5,52 @@ import Link from "next/link";
 
 export default function ProductCard({ product }) {
   return (
-    <div className="bg-white  rounded-lg shadow-md overflow-hidden">
-      {/* Product Image */}
-      <div className="relative">
-        <img
-          src={product.thumbnail}
-          alt="Product Image"
-          className="w-full h-64 object-cover"
-        />
-        {/* Discount Tag */}
-        <div className="absolute top-0 right-0 bg-red-500 text-white font-semibold py-1 px-3 rounded-bl-lg">
-          {product.discount}% OFF
-        </div>
-      </div>
-
-      {/* Product Details */}
-      <div className="p-4">
-        <Link
-          href={`/products/${product.slug}`}
-          className="text-lg font-semibold text-gray-800"
-        >
-          {product.product_name}
-        </Link>
-        <p className="text-sm text-gray-600 mt-1">{product.meta_description}</p>
-
-        {/* Price */}
-        <div className="mt-3 flex items-center">
-          <span className="text-xl font-bold text-gray-900">
-            {" "}
-            &#8377;
-            {paiseToRupee(discountedPrice(product.price, product.discount))}
-          </span>
-          <span className="text-sm text-gray-500 line-through ml-2">
-            &#8377;{paiseToRupee(product.price)}
-          </span>
+    <>
+      <div className=" bg-amber-400/20  rounded-lg shadow-md overflow-hidden mt-3 ">
+        {/* Product Image */}
+        <div className="relative ">
+          <img
+            src={product.thumbnail}
+            alt="Product Image"
+            className="w-full h-64 object-cover"
+          />
+          {/* Discount Tag */}
+          <div className="absolute top-0 right-0 bg-red-500 text-white font-semibold py-1 px-3 rounded-bl-lg">
+            {product.discount}% OFF
+          </div>
         </div>
 
-        {/* Add to Cart Button */}
-        <button className="mt-4 w-full bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-md flex items-center justify-center transition duration-300">
-          <ShoppingCart size={20} className="mr-2" />
-          Add to Cart
-        </button>
+        {/* Product Details */}
+        <div className="p-4  ">
+          <Link
+            href={`/products/${product.slug}`}
+            className="text-lg font-semibold text-gray-800 line-clamp-1 "
+          >
+            {product.product_name}
+          </Link>
+          <p className="text-sm text-gray-600 mt-1 line-clamp-3 ">
+            {product.meta_description}
+          </p>
+
+          {/* Price */}
+          <div className="mt-3 flex items-center  ">
+            <span className="text-xl font-bold text-gray-900 ">
+              {" "}
+              &#8377;
+              {paiseToRupee(discountedPrice(product.price, product.discount))}
+            </span>
+            <span className="text-sm text-gray-500 line-through ml-2">
+              &#8377;{paiseToRupee(product.price)}
+            </span>
+          </div>
+
+          {/* Add to Cart Button */}
+          <button className="mt-4 w-full bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-md flex items-center justify-center transition duration-300">
+            <ShoppingCart size={20} className="mr-2" />
+            Add to Cart
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
