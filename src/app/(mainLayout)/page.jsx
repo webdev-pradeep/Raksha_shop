07849c4a484c420361f5ca6753f7c1f3528da811
Pradeep <webdevpradeep0101@gmail.com>
2025-05-24 03:57:38 -1200

    Categories and thiers product fetched using API Calls

--- src/app/(mainLayout)/page.jsx
+++ src/app/(mainLayout)/page.jsx
@@ -41,33 +41,115 @@ const HomePage = () => {
   return (
     <>
       <div>
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+        <div className="pt-5 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
           {products.map((product) => (
             <ProductCard product={product} key={product.id} />
           ))}
         </div>
 
         <div className="m-5 p-5 shadow-2xl shadow-black  grid-cols-1  text-center ">
-          <h3 className="text-3xl font-bold ">Categories</h3>
-          <ul className=" list-none ">
+          <h3 className="text-3xl font-bold ">Shop By Category </h3>
+          <div className=" flex py-4 overflow-x-auto gap-4 ">
             {categories.map((category) => (
-              <li key={category.id} className="text-lg ">
-                <Link
-                  href={`/categories/${category.slug}`}
-                  className="text-blue-500 hover:underline"
-                >
-                  {category.name}
-                </Link>
-              </li>
+              <div
+                className="min-w-52 max-w-52 bg-white rounded-lg shadow-md overflow-hidden"
+                key={category.id}
+              >
+                {/* Product Image */}
+                <div className="flex justify-center items-center">
+                  <img
+                    src={category.image}
+                    alt="Product Image"
+                    className="w-36 h-36 rounded-full object-cover"
+                  />
+                </div>
+
+                {/* Product Details */}
+                <div className="p-4 text-center">
+                  <Link
+                    href={`/categories/${category.slug}`}
+                    className="font-medium text-gray-800"
+                    title={category.name}
+                  >
+                    {category.name}
+                  </Link>
+                </div>
+              </div>
             ))}
-          </ul>
+          </div>
         </div>
+      </div>
+
+      <div>
+        {categories.map((cat, index) => (
+          <>
+            {cat.products && (
+              <>
+                {cat.products.length > 0 && (
+                  <div className="px-5">
+                    <h3 className="mt-5 mb-2 font-medium text-2xl">
+                      {cat.name}
+                    </h3>
+                    <div className="flex py-4 overflow-x-auto gap-4">
+                      {cat.products.map((product) => (
+                        <div
+                          className="min-w-64 max-w-64 bg-white rounded-lg shadow-md overflow-hidden"
+                          key={product.id}
+                        >
+                          {/* Product Image */}
+                          <div className="relative">
+                            <img
+                              src={product.thumbnail}
+                              alt="Product Image"
+                              className="w-full h-48 object-cover"
+                            />
+                            {/* Discount Tag */}
+                            <div className="absolute top-0 right-0 bg-green-500 text-white font-semibold py-1 px-3 rounded-bl-lg">
+                              {product.discount}% OFF
+                            </div>
+                          </div>
+
+                          {/* Product Details */}
+                          <div className="p-4">
+                            <Link
+                              href={`/products/${product.slug}`}
+                              className="line-clamp-2 text-sm font-medium text-gray-800"
+                              title={product.product_name}
+                            >
+                              {product.product_name}
+                            </Link>
+                            {/* Price */}
+                            <div className="mt-2 flex items-center">
+                              <span className="text-xl font-bold text-gray-900">
+                                {" "}
+                                &#8377;
+                                {paiseToRupee(
+                                  discountedPrice(
+                                    product.price,
+                                    product.discount
+                                  )
+                                )}
+                              </span>
+                              <span className="text-sm text-gray-500 line-through ml-2">
+                                &#8377;{paiseToRupee(product.price)}
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                )}
+              </>
+            )}
+          </>
+        ))}
       </div>
 
       {loading && (
         <div className="flex items-center justify-center bg-amber-300 fixed inset-0 z-10 h-screen w-full ">
           <div className="relative">
-            <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200 text-5xl text-center">
+            <div className="h-24 w-24 rounded-full text-5xl text-center mt-5 ">
               𝓡𝓟
             </div>
             <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin"></div>
